622a17beedf2804311973e723cd44a07bec3e5e5
Yesha Pandya <yesha.pandya@outlook.com>
2024-06-04 17:10:53 +0530

    Create 01_hello_world.js

---
+++ 01_hello_world.js
@@ -0,0 +1,8 @@
+/*JAVASCRIPT display methods
+*Writing into an HTML element, using innerHTML.
+*Writing into the HTML output using document.write().
+*Writing into an alert box, using window.alert().
+*Writing into the browser console, using console.log().
+*/
+
+console.log("hello world")
